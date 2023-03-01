615132590ebced532f8abbb646a464e887473809
Marco 9726 <marcosarra9@gmail.com>
2023-03-01 17:06:34 +0100

    aggiunta messagio se viene creato un nuovo progetto

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -3,6 +3,11 @@
 @section('content')
 
 <div class="container p-4">
+	@if(session('message')) 
+		<div class="alert alert-success">
+			{{ session('message') }}
+		</div>
+	@endif
 	<div class="row flex-wrap gap-4 pt-3">
 		<a href="{{ route('admin.projects.create')}}" class="btn btn-small btn-success">Aggiungi progetto</a>
 		@foreach ($projects as $project)
